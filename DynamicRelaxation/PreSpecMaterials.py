4be8b821679acc423e5393b00c102aabe6861db4
paulnicholas <paul.nicholas@kadk.dk>
2013-11-09 22:56:56 +0100

    pn_update for julien

--- DynamicRelaxation/PreSpecMaterials.py
+++ DynamicRelaxation/PreSpecMaterials.py
@@ -1,4 +1,31 @@
 class Materials():
 
-    def steel(self, E = 2500, I = 1000, A = 1000): return ['STEEL', E, I, A]
-    def gfrp(self, E = 2100, I = 1000, A = 1000): return ['GFRP', E, I, A]
+    def __init__(self):    
+	self.E = 0
+	self.I = 0
+	self.A = 0
+	self.C = 0
+
+    def steel(self, E = 2500, I = 1000, A = 1000, C = 0): 
+	self.E = E
+	self.I = I
+	self.A = A
+	self.C = C
+	
+	return ['STEEL', E, A, I, C]
+
+    def gfrp(self, E = 2100, I = 1000, A = 1000, C = 0): 
+	self.E = E
+	self.I = I
+	self.A = A
+	self.C = C
+
+	return ['GFRP', E, A, I, C]
+
+    def membrane(self, E = 2100, I = 1000, A = 1000, C = 0.9): 
+	self.E = E
+	self.I = I
+	self.A = A
+	self.C = C
+
+	return ['MEMBRANE', E, A, I, C]
