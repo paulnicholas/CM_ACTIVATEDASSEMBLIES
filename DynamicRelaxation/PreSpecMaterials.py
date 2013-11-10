c95d042d97f1d42b756938f30591118d045741ce
paulnicholas <paul.nicholas@kadk.dk>
2013-11-10 23:14:29 +0100

    pn_update

--- DynamicRelaxation/PreSpecMaterials.py
+++ DynamicRelaxation/PreSpecMaterials.py
@@ -1,31 +1,54 @@
 class Materials():
 
     def __init__(self):    
-	self.E = 0
-	self.I = 0
-	self.A = 0
-	self.C = 0
+        self.E = -1
+        self.A = -1
+        self.I = -1
+        self.C = -1
+        self.D = -1
+        self.name = -1
 
-    def steel(self, E = 2500, I = 1000, A = 1000, C = 0): 
-	self.E = E
-	self.I = I
-	self.A = A
-	self.C = C
-	
-	return ['STEEL', E, A, I, C]
+    def steel(self, NAME = 'STEEL', E = 2500, I = 1000, A = 1000, C = 1, D = 1): 
+        self.E = E
+        self.A = A
+        self.I = I
+        self.C = C
+        self.D = D
+        self.name = NAME
+        return [self.name, self.E, self.A, self.I, self.C, self.D]
 
-    def gfrp(self, E = 2100, I = 1000, A = 1000, C = 0): 
-	self.E = E
-	self.I = I
-	self.A = A
-	self.C = C
+    def gfrp(self, NAME = 'GFRP', E = 2100, I = 1000, A = 1000, C = 1, D = 1): 
+        self.E = E
+        self.A = A
+        self.I = I
+        self.C = C
+        self.D = D
+        self.name = NAME
+        return [self.name, self.E, self.A, self.I, self.C, self.D]
 
-	return ['GFRP', E, A, I, C]
-
-    def membrane(self, E = 2100, I = 1000, A = 1000, C = 0.9): 
-	self.E = E
-	self.I = I
-	self.A = A
-	self.C = C
-
-	return ['MEMBRANE', E, A, I, C]
+    def rattan(self, NAME = 'RATTAN', E = 2100, I = 1000, A = 1000, C = 1, D = 1): 
+        self.E = E
+        self.A = A
+        self.I = I
+        self.C = C
+        self.D = D
+        self.name = NAME
+        return [self.name, self.E, self.A, self.I, self.C, self.D]
+    
+    def pvc(self, NAME = 'PVC', E = 2100, I = 1000, A = 1000, C = 1, D = 1):
+        self.E = E
+        self.A = A
+        self.I = I
+        self.C = C
+        self.D = D
+        self.name = NAME
+        return [self.name, self.E, self.A, self.I, self.C, self.D]
+        
+    def membrane(self, NAME = 'MEMBRANE', E = 2100, I = 1000, A = 1000, C = 1.9, D = 1): 
+        self.E = E
+        self.A = A
+        self.I = I
+        self.C = C
+        self.D = D
+        self.name = NAME
+        return [self.name, self.E, self.A, self.I, self.C, self.D]
